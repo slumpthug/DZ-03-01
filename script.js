95d0f854d01f30fe690a57c67b6f77f246ec0648
slumpthug <gangguchi58@gmail.com>
2022-06-03 19:52:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+//Задание 1
+// let a = 10;
+// let b = 3;
+
+// console.log(a / b);
+
+//Задание 2
+// let a = 2**10;
+// let st = a;
+
+// console.log(st);
+
+//Задание 3
+// let a = 1;
+// let b = 2;
+// let c = ' белых медведей';
+
+// console.log("" + a + b + c);
+
+//Задание 4
+// let a = 5;
+// let b = 10;
+// let c = a + b;
+
+// console.log(c);
+
+//Задание 5
+// const x = 15 * (4 + 25 - 55); 
+
+// console.log(x);
+
+//Доп. задание 1;
+// let wight = 10;
+// let height = 23;
+// let s = 0.5 * height * wight;
+
+// console.log(s);
+
+//Доп. задание 2
+// let heightC = 10;
+// let dC = 4;
+// let v = dC * heightC;
+
+// console.log(v)
+
+//Доп. задание 3
+// let s = 2000000;
+// let p = 100 * 10;
+// let years = 5;
+// let perepl = years * s - p;
+
+// console.log(perepl);
